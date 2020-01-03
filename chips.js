a64773bac3172b7e827b0d480fd33101d8a978d6
Zachary <zachjoliver@gmail.com>
2020-01-02 23:57:52 -0800

    Add chips partial solve

---
+++ chips.js
@@ -0,0 +1,9 @@
+//first check if first stack is as at least as big as the other 2 stacks
+//when it is, return lower of 2nd and 3rd elements
+function solve(arr) {
+  if (arr[0] >= arr[1] && arr[0] >= arr[2]) {
+    return arr[1] > arr[2] ? arr[1] : arr[2];
+  } else {
+    if (arr[2] > arr[3])
+  }
+}
