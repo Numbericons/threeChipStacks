04a629a2a485ba28b3276a94c7556e112a83cb9f
Zachary <zachjoliver@gmail.com>
2020-01-03 12:37:26 -0800

    Fix to correctly account for step by step nature and add last index to avoid situation where elements are the same and minEl and maxEl results in same element reduced twice

--- chips.js
+++ chips.js
@@ -14,21 +14,43 @@
 //keep remainder
 //add to count the smaller of remainder above and third value
 
+// function solve(arr) {
+//   let maxVal = Math.max(arr[0], arr[1], arr[2]);
+//   let minVal = Math.min(arr[0], arr[1], arr[2]);
+//   let minEl = arr.indexOf(minVal);
+//   let maxEl = arr.indexOf(maxVal);
+
+//   let count = minVal;
+//   arr[maxEl] -= minVal;
+
+//   arr.splice(minEl,1);
+//   count += arr[0] < arr[1] ? arr[0] : arr[1];
+
+//   return count;
+// }
+
+//get largest and smallest non-zero vals
+//while max > 0 && min > 0
+//subtract 1 from both and increment count
+
 function solve(arr) {
-  let maxVal = Math.max(arr[0], arr[1], arr[2]);
-  let minVal = Math.min(arr[0], arr[1], arr[2]);
-  if (maxVal === minVal) return minVal;
-  let minEl = arr.indexOf(minVal);
-  let maxEl = arr.indexOf(maxVal);
+  let count = 0;
+  while (true) {
+    if (arr.length < 2) break;
+    let minVal = Math.min(...arr);
+    let minEl = arr.indexOf(minVal);
+    if (minVal < 1) {
+      arr.splice(minEl,1);
+      continue;
+    }
+    let maxVal = Math.max(...arr);
+    let maxEl = arr.lastIndexOf(maxVal);
 
-  let count = minVal;
-  arr[maxEl] -= minVal;
-
-  arr.splice(minEl,1);
-  count += arr[0] < arr[1] ? arr[0] : arr[1];
-
+    count += 1;
+    arr[minEl] -= 1;
+    arr[maxEl] -= 1;
+  }
   return count;
 }
-
-let result = solve([1, 1, 1]);
+let result = solve([8, 2, 8]);
 console.log(result);
